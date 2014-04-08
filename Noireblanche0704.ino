7d183d199a3fd6ca333262c2c3740bba0d3ff8f8
Franck Brazier <franck.brazier@wanadoo.fr>
2014-04-08 11:52:14 +0200

    Modification du code

--- Noireblanche0704.ino
+++ Noireblanche0704.ino
@@ -1,8 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 #define PINBL 12
+#define NBLEDBL 21
 #define PINNO 10
+#define NBLEDNO 15
 #define NBLED 21
 #define FBLED 21
+
 Adafruit_NeoPixel blanche = Adafruit_NeoPixel(21, PINBL, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel noire = Adafruit_NeoPixel(15, PINNO, NEO_GRB + NEO_KHZ800);
 
@@ -15,45 +18,55 @@ int compt1 = 0;
 int compt2 = 0;
 char k =0 ;
 boolean premier =true;
+int nbdenotesrecu = 0;
 
 
 
 void setup() {
   Serial2.begin(9600); //MateGold 9600 SU,<value>
   Serial.begin(9600);
-  noire.begin(); blanche.begin(); delay(15000);
-  noire.show();blanche.show();// Initialize all pixels to 'off'
-  
+  noire.begin(); 
+  blanche.begin(); 
+  noire.show();
+  blanche.show();// Initialize all pixels to 'off'
+
 }
+
+
 
 void loop() { 
   while(Serial2.available()>0){
-  buffer[compt1]=Serial2.read();Serial.print(buffer[compt1]);compt1++;delay(2);}
-  raffraichissement = (long) buffer[0]*100; 
-  maj();
+    k=Serial2.read();
+    switch(k){
+      //Fin de gamme : '.'(46)
+      //Penser a une fonction d'erreur (renvoie des notes si le nb de notes n'est pas correcte)
+      case 46: Serial.print(nbdenotesrecu); nbdenotesrecu =0 ;return;
+     // Début de la transmission : ':'(58)
+      case 58:nbdenotesrecu =0;
+               raffraichissement = (long) Serial2.read()*100;break;
+      ///Arret : '!' (33)
+      case 33: arret();return;
+      //Pause : '-'(45)
+      case 45: pause();return;
+      //Fin du morceau : '/'(47)
+      case 47 :fin();return;  
+      //Play : '>' (62)
+      //devra etre effectuer apres l'envoi de la premiere ligne de notes a jouer
+      case 62 : return;
+      default : buffer[nbdenotesrecu];nbdenotesrecu ++ ; break;
+    }
+  }
   afficher();
 }
 
 
 void afficher(){ 
   if(millis()-chrono>raffraichissement){
-     maj();
+      blanche.show();
+      noire.show();
+      chrono=millis();
+    }
   }
-  else{ if(premier){int z=0;
-    for(int i=0;i<FBLED;i++){
-      if (i%2==1){noire.setPixelColor(i,couleurn(myarray2[i-z]));}
-        else{blanche.setPixelColor(i/2+1,couleurb(myarray2[i-z]));
-      }
-      if(i == 6||i==14 ||i==20 ||i==28||i==34||i==42){z++;
-    }blanche.setPixelColor(i/2+1,couleurb(myarray2[i-z]));}
-    blanche.show();
-    noire.show();
-    premier=false;
-    Serial2.print('*');delay(5);
-}}
-  while(Serial2.available()>0){
-  buffer[compt1]=Serial2.read();Serial.print(buffer[compt1]);compt1++;delay(2);}
-}
 
 
 
@@ -89,25 +102,40 @@ int32_t couleurn(int j){
   } 
 }
 
-void maj(){int j=0;chrono=millis();
- while(j<NBLED){ //Serial.print(j);
- if((NBLED+1)*compt2 + j+1<compt1){
-    k = buffer[(NBLED+1)*compt2 + j+1];}else{k=0;Serial.print("attente");}
-    
-    if (k==47){for(int v=0;v<NBLED;v++){
-      myarray2[v]=0;}
-     for (int zero=0; zero<(NBLED+1)*100;zero++){buffer[zero]=0;}
-      compt2=0;compt1=0;
-    return;}
-    else {
-        myarray2[j]=k;
-         j++;
+void arret(){
+  for(int i=0;i<NBLEDNO;i++){
+        blanche.setPixelColor(i,couleurb(0));
+        noire.setPixelColor(i,couleurn(0));
       }
-    }
-  if (j!=NBLED-1 && buffer[(NBLED+1)*compt2 + j+1]){Serial2.print('?');}
- 
-    compt2++;
-    premier=true;
+  for(int i=NBLEDNO;i<NBLEDBL;i++){   
+   blanche.setPixelColor(i,couleurb(0));}
+      blanche.show();
+      noire.show();
+}
+//void play(){}
+void pause(){while(Serial2.read()!=62){}
+}
+void fin(){  for(int i=0;i<NBLEDNO;i++){
+        blanche.setPixelColor(i,couleurb(0));
+        noire.setPixelColor(i,couleurn(0));
+      }
+  for(int i=NBLEDNO;i<NBLEDBL;i++){   
+   blanche.setPixelColor(i,couleurb(0));}
+}
+void miseentampon(){
+  int z =0;
+   for(int i=0;i<FBLED;i++){
+        if (i%2==1){
+          noire.setPixelColor(i,couleurn(buffer[i-z]));
+        }
+        else{
+          blanche.setPixelColor(i/2+1,couleurb(buffer[i-z]));
+        }
+        if(i == 6||i==14 ||i==20 ||i==28||i==34||i==42){
+          z++;
+        }
+        blanche.setPixelColor(i/2+1,couleurb(buffer[i-z]));
+      }
 }
 
 
